6721a87ccaaa0075107d39dfaa9312f6c27526fe
wabuyajames@gmail.com <wabuyajames@gmail.com>
2021-09-19 10:33:48 +0300

    Blog Changes

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 #admin.site.register(Post)
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'status')
     list_filter = ('status', 'created', 'author')
     search_fields = ('title', 'body')
-    prepopulated_fields = {'slug':('title')}
+    prepopulated_fields = {'slug':('title',)}
+    row_id_fields = ('author',)
+    date_hierarchy = 'created'
+    ordering = ('status',)
